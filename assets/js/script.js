c7491fd82333518fea563ae19588d390681ec70f
Mike <nguyen.mike1092@gmail.com>
2024-01-06 14:59:37 -0500

    fixed why getting error 400, had the same id in two places

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,45 @@
 //=============================== DOM Traversal ======================================//
 
-let city = $("#city");
-let submitBtn = $("#search");
+let cityName = $("#city");
+let submitBtn = $("#searchBtn");
 
 //================================ Main function ====================================//
 
+function getGeoCode() {
+
+    if (cityName !== "") {
+        
+        fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.val().trim()}&limit=1&appid=28a50b02ae4b700f3cf73b5f494e201a`
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                for (let i = 0; i < data.length; i++) {
+                    console.log(data[i].lat);
+                    console.log(data[i].lon);
+                }
+            });
+        } 
+} // END - getGeoCode //
+
+
+
+
 // function to get geocoding API
 
-submitBtn.on("click", function(event){
-    event.preventDefault()
-   let cityName = city.val();
+submitBtn.on("click", function (event) {
+    event.preventDefault();
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=28a50b02ae4b700f3cf73b5f494e201a`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-        console.log(data[i].lat);
-        console.log(data[i].lon);
-        }
-    })
-})
+    getGeoCode();
+    
+});
 
 /* 
 create function to get the lat and long from city with weather API
 */
-
 
 /* 
 create function to display recent 10 history (go to pokemon to display from array)
