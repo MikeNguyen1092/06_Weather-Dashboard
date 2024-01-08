2d9d097f968157c8ab9d5432fe915a3a769939b3
Mike <nguyen.mike1092@gmail.com>
2024-01-07 22:16:16 -0500

    made code cleaner by runing both todayweather and 5dayforecast at the same time

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,14 @@ addToStoredCities = []
 
 //================================ Main function ====================================//
 
-const getGeoCodeAndForecast = async (aCity) => {
+const getData = async (city) => {
+    const coordinates = await getGeoCode(city);
+    forecast(coordinates);
+    fiveDay(coordinates);
+
+};
+
+const getGeoCode = async (aCity) => {
     let geoResponse = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${aCity}&limit=1&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
@@ -24,6 +31,17 @@ const getGeoCodeAndForecast = async (aCity) => {
     let lat = geoData[0].lat;
     let lon = geoData[0].lon;
 
+    console.log(lat);
+    console.log(lon);
+
+    return [lat, lon]
+};
+// END - getGeoCode //
+
+const forecast = async (coordinates) => {
+    let lat = coordinates[0];
+    let lon = coordinates[1];
+    
     let weatherResponse = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
@@ -34,25 +52,21 @@ const getGeoCodeAndForecast = async (aCity) => {
         todayWind.text(weatherData.wind.speed)
         todayHumidity.text(weatherData.main.humidity)
 
-
-
     console.log(weatherData);
+}
 
-    let fiveDayResponse = await fetch(
+const fiveDay = async (coordinates) => {
+    let lat = coordinates[0];
+    let lon = coordinates[1];
+
+        let fiveDayResponse = await fetch(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
 
     let fiveDayData = await fiveDayResponse.json();
 
-    console.log("5 day forecast for - " + fiveDayData.city.name);
-};
-// END - getGeoCodeAndForecast //
-
-
-const forecast = async () => {
-    const result = await getGeoCodeAndForecast(citySearch.val());
-};
-
+    console.log(fiveDayData);
+}
 
 //========== Display Cities on page function  =========//
 
@@ -66,7 +80,7 @@ const displayCities = () => {
                 `<button type="button" class="btn btn-secondary mt-1"> ${city} </button>`
             );
             listCities.on("click", () => {
-                getGeoCodeAndForecast(city);
+                getData(city);
                 console.log("recent searched - " + city);
             });
             history.append(listCities);
@@ -93,7 +107,7 @@ const addToRecentSearches = () => {
 
 submitBtn.on("click", function (event) {
     event.preventDefault();
-    forecast();
+    getData(citySearch.val());
     addToRecentSearches();
     citySearch.val("");
 });
