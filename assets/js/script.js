3e03462b664bcd6fdadaeef85b0c1d3e1478b166
Mike <nguyen.mike1092@gmail.com>
2024-01-07 23:08:59 -0500

    some cleanups

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,7 @@ let todayTemp = $("#temp");
 let todayWind = $("#wind");
 let todayHumidity = $("#humidity");
 
-
 //=============================== Global Variables ===================================//
-
-addToStoredCities = []
 
 //================================ Main function ====================================//
 
@@ -19,60 +16,54 @@ const getData = async (city) => {
     const coordinates = await getGeoCode(city);
     forecast(coordinates);
     fiveDay(coordinates);
-
 };
 
+//========== Get lon and Lat from city  =========//
 const getGeoCode = async (aCity) => {
     let geoResponse = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${aCity}&limit=1&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
 
     let geoData = await geoResponse.json();
+
     let lat = geoData[0].lat;
     let lon = geoData[0].lon;
 
-    console.log(lat);
-    console.log(lon);
-
-    return [lat, lon]
+    return [lat, lon];
 };
 // END - getGeoCode //
 
 const forecast = async (coordinates) => {
-    let lat = coordinates[0];
-    let lon = coordinates[1];
-    
     let weatherResponse = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=28a50b02ae4b700f3cf73b5f494e201a`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
     let weatherData = await weatherResponse.json();
 
-        nameOfCity.text(weatherData.name);
-        todayTemp.text(weatherData.main.temp)
-        todayWind.text(weatherData.wind.speed)
-        todayHumidity.text(weatherData.main.humidity)
+    nameOfCity.text(weatherData.name);
+    todayTemp.text(weatherData.main.temp);
+    todayWind.text(weatherData.wind.speed);
+    todayHumidity.text(weatherData.main.humidity);
 
     console.log(weatherData);
-}
+};
 
 const fiveDay = async (coordinates) => {
-    let lat = coordinates[0];
-    let lon = coordinates[1];
-
-        let fiveDayResponse = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=28a50b02ae4b700f3cf73b5f494e201a`
+    let fiveDayResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=28a50b02ae4b700f3cf73b5f494e201a`
     );
 
     let fiveDayData = await fiveDayResponse.json();
 
     console.log(fiveDayData);
-}
+};
 
 //========== Display Cities on page function  =========//
 
 const displayCities = () => {
     // Clears the previous buttons before calling localStorage to re-add them
     history.empty();
+
+    // Get cities from local storage, make it a btn, put a handler, and put it on the page
     let storedCities = JSON.parse(localStorage.getItem("cities"));
     if (storedCities) {
         storedCities.forEach((city) => {
@@ -105,8 +96,11 @@ const addToRecentSearches = () => {
 };
 // END - addToRecentSearches //
 
+// ===== Handler ===== //
+
 submitBtn.on("click", function (event) {
     event.preventDefault();
+    // Get the value from the input section from html
     getData(citySearch.val());
     addToRecentSearches();
     citySearch.val("");
